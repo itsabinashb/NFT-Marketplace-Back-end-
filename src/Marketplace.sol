3d6ac7cb4d20bed041c0339d173946be8dc122bc
ABINASH BURMAN <abinash@hacek>
2023-04-27 21:29:30 +0530

    final

--- src/Marketplace.sol
+++ src/Marketplace.sol
@@ -16,6 +16,13 @@ contract Marketplace is ERC721, IERC721Receiver {
     address public owner;
     uint256 targetBalance = 10 ether;
     uint256 maxPriceChange = 10;
+    uint256 timeLimit;
+
+    event TokenMinted(uint256 tokenId);
+    event MarketCreated(uint256 tokenId, uint256 price);
+    event ItemPurchased(uint256 tokenId, address buyer);
+    event PurchasedItemSold(uint256 tokenId, address seller);
+    event OnERC721ReceivedTriggered(address, address, uint256, bytes);
 
     struct Item {
         uint256 _tokenId;
@@ -27,12 +34,12 @@ contract Marketplace is ERC721, IERC721Receiver {
 
     mapping(uint256 => Item) public IdToItem;
     mapping(uint256 => bool) public readyToSell;
-    mapping(uint256 => bool) public sold;
     mapping(uint256 => address) public tokenBuyer;
 
     constructor(string memory _baseUri) ERC721("GOOD_TOKEN", "GT") {
         baseUri = _baseUri;
         owner = msg.sender;
+        timeLimit = block.timestamp + 120 days;
     }
 
     /**
@@ -43,7 +50,8 @@ contract Marketplace is ERC721, IERC721Receiver {
         tokenId.increment();
         uint256 currentTokenId = tokenId.current();
         _safeMint(msg.sender, currentTokenId);
-
+        tokenURI(currentTokenId);
+        emit TokenMinted(currentTokenId);
         return currentTokenId;
     }
 
@@ -52,18 +60,19 @@ contract Marketplace is ERC721, IERC721Receiver {
         address from,
         uint256 _tokenId,
         bytes calldata data
-    ) external view override returns (bytes4) {
+    ) external override returns (bytes4) {
         require(msg.sender == address(this));
-
+        emit OnERC721ReceivedTriggered(operator, from, _tokenId, data);
         return this.onERC721Received.selector;
     }
 
     /**
-     * @dev set the token in marketplace contract
+     * @dev set the token in marketplace contract for sell
      * @param _tokenId tokenId of token which is going to be submitted in contract
      * @param _price  price of the token set by caller
      */
-    function createMarketItem(uint256 _tokenId, uint256 _price) public {
+    function createMarketItem(uint256 _tokenId, uint256 _price) public payable {
+        require(msg.value == listPrice, "Pay 0.01 ether to set for sale");
         require(
             IdToItem[_tokenId].owner == msg.sender,
             "You are not the owner of this token"
@@ -78,6 +87,7 @@ contract Marketplace is ERC721, IERC721Receiver {
             false
         );
         readyToSell[_tokenId] = true;
+        emit MarketCreated(_tokenId, _price);
     }
 
     /**
@@ -92,14 +102,17 @@ contract Marketplace is ERC721, IERC721Receiver {
         require(sent);
         (bool success, ) = IdToItem[_tokenId].seller.call{value: msg.value}("");
         require(success);
-        sold[_tokenId] = true;
+
         IdToItem[_tokenId].sold = true;
         readyToSell[_tokenId] = false; // tracking unsold tokens
         soldTokenId.increment();
         tokenBuyer[_tokenId] = msg.sender;
         tokenId.decrement();
+        emit ItemPurchased(_tokenId, msg.sender);
     }
-
+     /**
+      * @dev function for getting unsold items of marketplace
+      */
     function getUnsoldItems() public view returns (Item[] memory) {
         uint256 totalTokenNumber = tokenId.current();
 
@@ -108,6 +121,7 @@ contract Marketplace is ERC721, IERC721Receiver {
         for (uint256 i = 1; i <= totalTokenNumber; i++) {
             if (readyToSell[i] == true) {
                 require(IdToItem[i].owner == address(this));
+                require(!IdToItem[i].sold);
                 Item storage currentId = IdToItem[i];
                 items[arrayIndex] = currentId;
                 arrayIndex++;
@@ -117,6 +131,9 @@ contract Marketplace is ERC721, IERC721Receiver {
         return items;
     }
 
+    /**
+     * @dev function for getting items of particular creator
+     */
     function getYourItems() public view returns (Item[] memory) {
         uint256 totalTokenNumber = tokenId.current();
 
@@ -131,7 +148,9 @@ contract Marketplace is ERC721, IERC721Receiver {
         }
         return yourItems;
     }
-
+    /**
+     * @dev returns all items purchased by buyer
+     */
     function returnPurchasedItems() public view returns (Item[] memory) {
         uint256 totalTokenNumber = tokenId.current();
         uint256 arrayIndex = 0;
@@ -159,8 +178,15 @@ contract Marketplace is ERC721, IERC721Receiver {
         }("");
         require(success);
         tokenId.increment();
+        emit PurchasedItemSold(_tokenId, msg.sender);
     }
 
+    /**
+     * @dev algorithm for generating price based on contract's balance, here seller's loss or profit will be depended on the balance of contract. If balance of contract is
+     * less than target balance then seller will get less price than item's previous price and if the balance of contract is greater than target balance then seller will get
+     * more price than item's previous price.
+     * @param _tokenId tokenId
+     */
     function generatePriceForToken(uint256 _tokenId) public returns (uint256) {
         uint256 deviation = address(this).balance - targetBalance;
         uint256 price;
@@ -180,10 +206,9 @@ contract Marketplace is ERC721, IERC721Receiver {
             uint256 maxPriceDecrease = (
                 IdToItem[_tokenId].price.mul(maxPriceChange)
             ).div(100);
-            uint256 priceDecrease = (
-                (0 - deviation).mul(IdToItem[_tokenId].price)
-            ).div(targetBalance);
-            if (priceDecrease > maxPriceDecrease) {
+            uint256 priceDecrease = (deviation.mul(IdToItem[_tokenId].price))
+                .div(targetBalance);
+            if (priceDecrease < maxPriceDecrease) {
                 priceDecrease = maxPriceDecrease;
             }
             price = IdToItem[_tokenId].price -= priceDecrease;
@@ -191,4 +216,28 @@ contract Marketplace is ERC721, IERC721Receiver {
 
         return price;
     }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseUri;
+    }
+
+    /**
+     * @dev owner can withdraw eth every 4 months to keep the balance of contract stable so that sellers can make profit.
+     */
+    function withdraw() public onlyOwner {
+        require(block.timestamp == timeLimit);
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success);
+        timeLimit = block.timestamp + 120 days;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+     * @dev in case contract might need external funding to keep eth amount stable
+     */
+    receive() external payable {}
 }
